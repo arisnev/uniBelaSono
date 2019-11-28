0101d138a859fb9c916837c0627edd322cee9a1b
Венера Мустафина < >
2019-11-29 02:38:46 +0500

    Header, First-page

---
+++ www/index.php
@@ -0,0 +1,118 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="keyword" conent="концерт, Челябинск"/>
+        <meta name="description" content="Концерты BelaSono в Челябиске"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="img/favicon.png" type="image/png">
+
+        <title>BelaSono</title>
+    </head>
+
+    <body>
+        <header>
+            <figure class="logo"><a href="#"><img src="img/BlackLogo.png"></a></figure>
+            <div id="nav">
+                <ul id="buttom-block">
+                    <li id="about">
+                        <a href="" title="Перейтит к описанию"><b>о нас</b></a><!--href="/about/artists"-->
+                    </li>
+                    <li id="gallery">
+                        <a href="" title="Перейтит к галлерее"><b>галлерея</b></a>
+                    </li>
+                    <li id="in">
+                        <a href="" title="Войти"><b>вход</b></a>
+                    </li>
+                    <li id="reg">
+                        <a href="" title="Зарегестрироваться"><b><font color="#ffa642">регистрация</font></b></a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+
+        <menu>
+            <div id="first-page">
+                <div id="inviting">
+                    <span id="december">мы ждем тебя в декабре</span></p>
+                    <span id="press-here">жми
+                        <span id="here">здесь</span>
+                        , чтобы узнать больше
+                    </span></p>
+                </div>
+                <div id="sofar">Inspired by Sofar</div>
+            </div>
+
+            <div id="second-page">
+                <article>
+                    <h1>Как к нам попасть?</h1>
+                    <section id="first-step">
+                        <figure id="first-pict"><img src="img/first-step.png"></figure>
+                        <div id="text1">
+                            <h2>1. Отправь заявку</h2>
+                            Наше шоу - это небольшая лотерея.  Успей подать заявку за неделю до начала мероприятия. билеты на концерт получают те, кто был выбран беспристрастным рандомом или же поведал нам вескую причину. Подробную информацию можно найти здесь.
+                        </div>
+                    </section>
+                    <section id="second-step">
+                        <figure id="second-pict"><img src="img/second-step.png"></figure>
+                        <div id="text2">
+                            <h2>2. Пригласи друзей</h2>
+                            При регистрации ты можете указать +1 для своего друга, но шансов будет больше, если он зарегестрируется вместе с тобой. </p>Ты также можешь прийти один - никогда не знаешь, с кем встретишься!
+                        </div>
+                    </section>
+                    <section id="third-step">
+                        <figure id="third-pict"><img src="img/third-step.png"></figure>
+                        <div id="text3">
+                            <h2>3. Наслаждайся вечером</h2>
+                            Мы отправим тебе электронное письмо за день до начала шоу с адресом. Приходи с напитками и выбирай место поудобнее (напольное сидение приветствуется!). Ты не узнаеешь, кто играет, пока не увидишь сам. Возможно, ты откроешь для себя новых любимых исполнителей!
+                        </div>
+                    </section>
+                </article>
+            </div>
+        </menu>
+
+        <footer>
+            <div id="footerleft">
+                <img src="img/WhiteLogo.png">
+                <ul id="reg-enter">
+                    <li id="enter">
+                        Вход
+                    </li>
+                    <li id="reg">
+                        Регистрация
+                    </li>
+                </ul>
+            </div>
+            <div id="footerright">
+                <div class="address">
+                    <font color="ffa642">АДРЕС:</font>
+                </div>
+                <div class="address-mail">
+                    челябинск, проспект ленина, 80</p>
+                     <font color="ffa642">e-mail:</font> random.address@gmail.com
+                </div>
+                <div class="networks">
+                    <img src="img/VK.png" alt="Вконтакте" title="Вконтакте"/>
+                    <img src="img/Youtube.png" alt="Youtube" title="Youtube"/>
+                    <img src="img/Insta.png" alt="Instagram" title="Instagram"/>
+                </div>
+            </div>
+            <div id="footercenter">
+                <ul id="menu-footer">
+                    <li id="about">
+                        <a href="#about" title="Нажмите, чтобы перейти"><b>О нас</b></a>
+                    </li>
+                    <li id="gallery">
+                        <a href="#gallery" title="Нажмите, чтобы перейти"><b>Правила</b></a>
+                    </li>
+                    <li id="in">
+                        <a href="#in" title="Нажмите, чтобы перейти"><b>Контакты</b></a>
+                    </li>
+                    <li id="reg">
+                        <a href="#reg" title="Нажмите, чтобы перейти"><b>FAQ</b></a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </body>
+</html>
